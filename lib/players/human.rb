6a109e8e6fdebd7d9e550805ca86d608f9701ed1
eulis01 <eulis01@gmail.com>
2020-08-29 13:17:40 -0400

    add required relative and def typo fix

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+require_relative '../player.rb'
 module Players
   class Human < Player
-    def move(_board)
-      puts 'Please enter 1-9:'
-      gets.strip
+    def move(board)
+      puts("Enter Input.")
+      input = gets.strip
     end
   end
 end
